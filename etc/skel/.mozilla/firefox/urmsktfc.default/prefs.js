79807cab536bae0d9bba67f7fa2963a277c44336
Leszek Lesner <leszek.lsner@web.de>
2017-02-20 16:43:53 +0100

    Disabling multiprocess support as it is messing with addons

--- etc/skel/.mozilla/firefox/urmsktfc.default/prefs.js
+++ etc/skel/.mozilla/firefox/urmsktfc.default/prefs.js
@@ -415,3 +415,4 @@ user_pref("xpinstall.whitelist.add", "");
 user_pref("xpinstall.whitelist.add.180", "");
 user_pref("xpinstall.whitelist.add.36", "");
 user_pref("browser.disableResetPrompt", true);
+user_pref("browser.tabs.remote.autostart2", false);
